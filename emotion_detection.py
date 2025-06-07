559d770a998517ccb144485316ae090e4c76e574
k221024 <kritie2210@gmail.com>
2025-06-07 16:59:00 +0530

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -13,5 +13,24 @@ def emotion_detector(text_to_analyse):
     
     #Send a POST request to the API with the text and headers
     response = requests.post(url, json = myobj, headers=header)
+
+    #Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+    # Extracting emotions and their score from the response
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
     
-    return response.text 
+    #Collecting Emotions into single file
+    emotions = {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score}
+
+    #Finding the dominant emotion with maximum score
+    dominant_emotion_name = max(emotions, key=emotions.get)
+
+    #Adding dominant_emotion key and value
+    emotions['dominant_emotion'] = dominant_emotion_name
+
+    return emotions
